5cace65d4b1e582ae27a227be499891385c43d2a
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-08-12 14:09:05 +0300

    правки по замечаниям

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-import {getTripInfo} from `./components/trip-info`;
-import {getMenu} from `./components/menu`;
-import {getFilters} from `./components/filters`;
-import {getSort} from `./components/sort`;
-import {getForm} from `./components/form`;
-import {getCard} from `./components/card`;
+import {getTripInfo} from './components/trip-info';
+import {getMenu} from './components/menu';
+import {getFilters} from './components/filters';
+import {getSort} from './components/sort';
+import {getForm} from './components/form';
+import {getCard} from './components/card';
 
 const CARDS_AMOUNT = 3;
 
-const renderElement = function (selector, place, content, count = 1) {
-  for (let i = 0; i < count; i++) {
-    let container = document.querySelector(selector);
-
-    container.insertAdjacentHTML(place, content);
-  }
+const renderElement = function (selector, position, content) {
+  const container = document.querySelector(selector);
+  container.insertAdjacentHTML(position, content);
 };
 
 renderElement(`.trip-main__trip-info`, `afterbegin`, getTripInfo());
@@ -20,4 +17,7 @@ renderElement(`.trip-main__trip-controls > h2`, `afterend`, getMenu());
 renderElement(`.trip-main__trip-controls`, `beforeend`, getFilters());
 renderElement(`.trip-events`, `beforeend`, getSort());
 renderElement(`.trip-events`, `beforeend`, getForm());
-renderElement(`.trip-events`, `beforeend`, getCard(), CARDS_AMOUNT);
+
+for (let i = 0; i < CARDS_AMOUNT; i++) {
+  renderElement(`.trip-events`, `beforeend`, getCard());
+}
